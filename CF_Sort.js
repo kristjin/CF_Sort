7cea9177b115968a2805532f5be3807ef9b61e6e
Matthew Conquergood <matthew.conquergood@gmail.com>
2014-10-05 22:53:53 -0700

    updates Readme, cleans up code slightly for reading.

--- CF_Sort.js
+++ CF_Sort.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 
   var $holdingDiv = $('#holding').hide();
   var $hdi = $('#holding>img');
-  var $hdiSrc = $hdi.attr('src');
   var $i, $m, $mSrc, c, $c, $cSrc, m;
 
   function Deck() {
@@ -54,8 +53,6 @@ $(document).ready(function(){
     $img.attr('src', toThis);
   }
 
-
-
   function shuffle(m){                                      //modifying code from class
     var rand = Math.floor(Math.random() * m);               //get a random number between 0 and the number passed as m
     var $rand;                                              //gets used later
@@ -82,72 +79,57 @@ $(document).ready(function(){
     }
   }
 
-  function sort(){                                          //sort all cards for m
+  function sort(){                                //sort all cards for m
     $holdingDiv.show();
     m=1;
     c=0;
-    $m = $('.card:eq(' + m + ')>img');                    //get the img element for the $m card
+    $m = $('.card:eq(' + m + ')>img');            //get the img element for the $m card
     $mSrc = $m.attr('src');
     $i = $m;
     var mIsSorted=false;
-    console.log('calling timerID loop');
     var timerID = setInterval(function(){
-      console.log('timerID is looping again, m is: ' + m);
       if (m>51) {
+        $holdingDiv.hide();
         clearInterval(timerID);
       } else if (mIsSorted) {
-        console.log(m+'th card has been sorted');
         m++;
-        console.log('m is now: ' + m);
         c = (m-1);
-        console.log('and c is now: ' + c);
-        $m = $('.card:eq(' + m + ')>img');                    //get the img element for the $m card
-        $mSrc = $m.attr('src');                               //get the src for the $m card image
-        console.log('updated $mSrc to: ' + $mSrc);
-        change($hdi, $mSrc);                                      //set the holding div img to the mth card img
-        $i = $m;                                              //insert point, begins at the mth position
-        blank($i);                                                //set the $i (insert point) to blank card
+        $m = $('.card:eq(' + m + ')>img');        //get the img element for the $m card
+        $mSrc = $m.attr('src');                   //get the src for the $m card image
+        change($hdi, $mSrc);                      //set the holding div img to the mth card img
+        $i = $m;                                  //insert point, begins at the mth position
+        blank($i);                                //set the $i (insert point) to blank card
         mIsSorted=false;
-        console.log('mIsSorted set to false');
-      } else if (c==-1) {       //you're at the top of the list so put m in position 1
-        console.log('at top of list c=0');
+      } else if (c==-1) {                         //you're at the top of the list so put m in position 0
         $i = $('.card:eq(0)>img');
         change($i, $mSrc);
         blank($hdi);
         mIsSorted=true;
-        console.log('set mIsSorted to true.')
       } else {
-        console.log('not at top of list, so comparing');
-        console.log('c is: '+c);
-        $c = $('.card:eq(' + c + ')>img');                        //set $c to the cth card
-        $cSrc = $c.attr('src');                                   //and get its src
-        console.log('$cSrc is: '+$cSrc);
-        console.log('$mSrc is: ' + $mSrc);
-        if(en($mSrc)<en($cSrc)){                                  //if m rank < c rank
-          console.log('*** m<c so butt down');
-          change($i, $cSrc);                                        //Change $i (blank) to $c img
-          blank($c);                                                //blank the comparison point
-          $i = $c;                                                  //set the insert point to the comparison point
+        $c = $('.card:eq(' + c + ')>img');        //set $c to the cth card
+        $cSrc = $c.attr('src');                   //and get its src
+        if(en($mSrc)<en($cSrc)){                  //if m rank < c rank
+          change($i, $cSrc);                      //Change $i (blank) to $c img
+          blank($c);                              //blank the comparison point
+          $i = $c;                                //set the insert point to the comparison point
           c--;
-        } else {                                                  //if m rank > c rank, then m belongs in $i so
-          console.log('*** m>c so m belongs in $i');
-          change($i, $mSrc);                                        //move m into to the insert point
-          blank($hdi);                                              //blank the holding div img
+        } else {                                  //if m rank > c rank, then m belongs in $i so
+          change($i, $mSrc);                      //move m into to the insert point
+          blank($hdi);                            //blank the holding div img
           mIsSorted=true;
         }
       }
-    }, 10);                                                  //end timerID (comparison) loop                                                          //end while loop, advancing M 1-52
-  }                                                         //end sort()
+    }, 250);                                      //end timerID (comparison) loop
+  }                                               //end sort()
 
 
   $('#shuffle').on('click', function() {
-    shuffle($(".card").length - 1);                         //call shuffle function passing length of deck -1 (0-array)
+    shuffle($(".card").length - 1);               //call shuffle function passing length of deck -1 (0-array)
   });
 
   $('#sort').on('click', function(){
     sort();
   });
-
 
   var deck = new Deck();
 
--- README.txt
+++ README.txt
@@ -4,3 +4,11 @@ I still do  not understand how 3 variables are passed to his use of setInterval.
 I  did the code for the insertion sort.  I chose insertion sort because it's extremely simple and therefore, in my mind,
 easier to implement on the screen.
 
+lol, what a great idea that it would be "easy"  It wasn't!
+
+I learned SO MUCH doing this project.  I still think there's a lot I can do to make this cooler.
+I may spend some time on it going forward for practice.
+
+I had a lot of issues with the timer function, as usual.  And a lot of scope  issues  I had  to learn to watch for.
+
+But in the end, this works, and is kinda neat.  I'd like to do more animationy stuff with it now.
